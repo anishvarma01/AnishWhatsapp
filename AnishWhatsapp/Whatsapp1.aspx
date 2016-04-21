32e5f357c65cd4fac05c6e18d96c76ebee31daff
anishvarma01 <anishvarma.net@gmail.com>
2016-04-21 12:46:12 -0400

    initial commit

---
+++ AnishWhatsapp/Whatsapp1.aspx
@@ -0,0 +1,92 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Whatsapp1.aspx.cs" Inherits="AnishWhatsapp.Whatsapp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 367px;
+        }
+        .Img {
+            background-image: url(Images/Whatsapp.jpg);
+            background-size: 100%;
+            
+        }
+    </style>
+</head>
+<body class="Img">
+    <form id="form1" runat="server">
+    <div>
+    
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label1" runat="server" Text="To"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label2" runat="server" Text="Message"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server" TextMode="MultiLine"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" Text="Send" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label3" runat="server" Text="Chatting"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server" Height="253px" TextMode="MultiLine" Width="244px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label4" runat="server" Text="From"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label5" runat="server" Text="Uid"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label6" runat="server" Text="PassWord"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td>
+                    <asp:Button ID="Button2" runat="server" Text="Connect" OnClick="Button2_Click" />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ AnishWhatsapp/Whatsapp1.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WhatsAppApi;
+using WhatsAppApi.Account;
+
+namespace AnishWhatsapp
+{
+    public partial class Whatsapp1 : System.Web.UI.Page
+    {
+        Whatsapp WA;
+        WhatsUser USER;
+        private delegate void UpdateTextBox(TextBox textbox, string value);
+        public void UpdateDataTextBox(TextBox textbox, string value)
+        {
+            textbox.Text += value;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBox2.Text))
+                return;
+            if(WA != null)
+            {
+                if(WA.ConnectionStatus == ApiBase.CONNECTION_STATUS.LOGGEDIN)
+                {
+                    WA.SendMessage(TextBox1.Text, TextBox2.Text);
+                    TextBox3.Text += string.Format("\r\n{0} :{1}", USER.Nickname, TextBox2.Text);
+                    TextBox2.Clear();
+                    TextBox2.Focus();
+                }
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            WhatsUserManager Manage = new WhatsUserManager();
+            USER = Manage.CreateUser(TextBox5.Text, TextBox6.Text);
+            var thread = new Thread(testc =>
+            {
+                UpdateTextBox textbox = UpdateDataTextBox;
+                WA = new Whatsapp(TextBox5.Text, TextBox6.Text, TextBox4.Text, true);
+                WA.OnConnectSuccess += () =>
+                {
+                    if (TextBox3.InvokeRequired)
+                        Invoke(textbox, TextBox3, "Connected....");
+                    WA.OnLoginSuccess += (phone, data) =>
+                    {
+                        if (TextBox3.InvokeRequired)
+                            invoke(textbox, TextBox3, "\r\nLogin Success....");
+                        while (WA != null)
+                        {
+                            WA = PollMessage();
+                            Thread.Sleep(200);
+                            continue;
+                        }
+                    };
+                    WA.OnGetMessage += (node, from, id, name, message, receipt_send) =>
+                    {
+                        if (TextBox3.InvokeRequired)
+                            invoke(textbox, TextBox3, string.Format("\r\n{0}:{1}", name, message));
+                    };
+                    WA.OnLoginFailed += (data) =>
+                    {
+                        if (TextBox3.InvokeRequired)
+                            invoke(textbox, TextBox3, string.Format("\r\nConnect failed: {0}", data));
+                    };
+                    WA.Login();
+                };
+                WA.OnConnectFailed += (ex) =>
+                {
+                    if (TextBox3.InvokeRequired)
+                        invoke(textbox, TextBox3, string.Format("\r\nConnection Failed {0}", ex.StactTrace));
+
+                };
+                           
+
+               
+                    WA.Connect();
+                }) { IsBackground = true };
+            thread.Start();
+        }
+    }
+}
